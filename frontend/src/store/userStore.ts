f6da5bb715907b5519fe63cfe2e56c08b0761a37
coreyan1234 <coreyan1234@gmail.com>
2024-04-11 21:18:40 -0400

    Set up the reducer and store. Need to set up the provider

---
+++ frontend/src/store/userStore.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import usersReducer from "../reducers/usersReducer";
+
+const store = configureStore({
+ reducer: {
+   users: usersReducer,
+ },
+});
+
+export default store;
