834c7647799b42cc248385363f161b643e3940a5
coreyan1234 <coreyan1234@gmail.com>
2024-04-12 22:04:17 -0400

    Migrated Login.tsx functions into firebase.ts

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-// If you want to add more firebase services, add them here
+import { Auth, getAuth } from "firebase/auth";
+import { User, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import axios from "axios";
 
 // Your web app's Firebase configuration
 // TODO: Put the apiKey in ENV
@@ -17,3 +18,46 @@ const firebaseConfig = {
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
+
+export const signIn = async (email: string, password: string) => {
+  // Firebase's signIn function
+  signInWithEmailAndPassword(firebaseAuth, email, password)
+  .then((userCredentials) => {
+      // console.log(`Signed In! UserCredentials: ${JSON.stringify(authUser)}`);
+  })
+  .catch((error) => {
+      console.log(error);
+  });
+
+  // Sending GET request to MongoDB endpoint to retrieve user. Ideally, we'd like to use
+  // the user's firebase uid rather than hardcoding their MongoDB user id
+  const response = await axios.get(`http://localhost:4000/api/users/6612e6d0f74acd763f3a7ccb`);
+  // console.log(`Got the user from MongoDB via axios! Response:${JSON.stringify(authUser)}`);
+}
+
+export const signUp = async (email: string, password: string, authUser: User | null) => {
+  // Firebase's register function
+  createUserWithEmailAndPassword(firebaseAuth, email, password)
+  .then((userCredentials) => {
+      console.log(`Signed Up! UserCredentials: ${JSON.stringify(userCredentials.user)}`);
+  })
+  .catch((error) => {
+      console.log(error);
+  });
+
+  const userData = {
+      Username: "Axios_Username",
+      Password: password,
+      Email: email,
+      role: 'axios_role',
+      uid: authUser?.uid
+  }
+
+  // Sending POST request to MongoDB endpoint to create new user
+  const response = await axios.post(`http://localhost:4000/api/users/register`, userData);
+  console.log(`Sent axios post request to register user! Response: ${JSON.stringify(response.data)}`);
+}
+
+export const logout = async (auth: Auth) => {
+  await signOut(auth);
+}
