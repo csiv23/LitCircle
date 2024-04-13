1e0352ecbbabda3ca5afc079958da15a1da21759
coreyan1234 <coreyan1234@gmail.com>
2024-04-12 22:04:17 -0400

    Migrated Login.tsx functions into firebase.ts

--- frontend/src/components/Login/index.tsx
+++ frontend/src/components/Login/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { firebaseAuth } from "../../firebase";
 import { User, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import axios from "axios";
+import * as firebaseClient from '../../firebase'
 
 function Login() {
     const [email, setEmail] = useState("")
@@ -26,56 +27,13 @@ function Login() {
         // Cleanup function
         return () => unsubscribe();
     }, [])
-
-    const signIn = async () => {
-        // Firebase's signIn function
-        signInWithEmailAndPassword(firebaseAuth, email, password)
-        .then((userCredentials) => {
-            console.log(`Signed In! UserCredentials: ${JSON.stringify(authUser)}`);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-
-        // Sending GET request to MongoDB endpoint to retrieve user. Ideally, we'd like to use
-        // the user's firebase uid rather than hardcoding their MongoDB user id
-        const response = await axios.get(`http://localhost:3001/api/users/6612e6d0f74acd763f3a7ccb`);
-        console.log(`Got the user from MongoDB via axios! Response:${JSON.stringify(authUser)}`);
-    }
-    const signUp = async () => {
-        // Firebase's register function
-        createUserWithEmailAndPassword(firebaseAuth, email, password)
-        .then((userCredentials) => {
-            console.log(`Signed Up! UserCredentials: ${JSON.stringify(userCredentials.user)}`);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-
-        const userData = {
-            Username: "Axios_Username",
-            Password: password,
-            Email: email,
-            role: 'axios_role',
-            uid: authUser?.uid
-        }
-
-        // Sending POST request to MongoDB endpoint to create new user
-        const response = await axios.post(`http://localhost:3001/api/users/register`, userData);
-        console.log(`Sent axios post request to register user! Response: ${JSON.stringify(response.data)}`);
-    }
-
-    const logout = async () => {
-        await signOut(firebaseAuth);
-    }
-
     
     return (
         <div>
             {isLoggedIn ? (
                 <>
                     <p>You are logged in!</p>
-                    <button onClick={logout}>Logout</button>
+                    <button onClick={() => firebaseClient.logout(firebaseAuth)}>Logout</button>
                 </>
             ) : (
                 <>
@@ -87,8 +45,8 @@ function Login() {
                         onChange={(e) => {
                             setPassword(e.target.value);
                         }}/>
-                    <button onClick={signIn}>Login</button>
-                    <button onClick={signUp}>Create Account</button>
+                    <button onClick={() => firebaseClient.signIn(email, password)}>Login</button>
+                    <button onClick={() => firebaseClient.signUp(email, password, authUser)}>Create Account</button>
                 </>
             )}
         </div>
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from './components/Login';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Login />
     <App />
   </React.StrictMode>
 );
