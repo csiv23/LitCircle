6cdeec483e8a3070aed5003f75805235090f51bb
coreyan1234 <coreyan1234@gmail.com>
2024-04-12 23:58:30 -0400

    Implemented CurrentUser via reducer. Currently keeps track of the user's firebase uid. TODO: Keep track of the user as MongoDB user instead of just the firebase uid

--- frontend/src/components/Login/index.tsx
+++ frontend/src/components/Login/index.tsx
@@ -3,34 +3,17 @@ import { firebaseAuth } from "../../firebase";
 import { User, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import axios from "axios";
 import * as firebaseClient from '../../firebase'
+import { useSelector } from "react-redux";
 
 function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const [authUser, setAuthUser] = useState<User | null>(null);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
     
-    useEffect(() => {
-        // onAuthStateChanged is always listening for changes in authentication state
-        // SO, authUser will always be up to date and you can just access information
-        // about the current user via authUser
-        const unsubscribe = onAuthStateChanged(firebaseAuth, user => {
-            if (user) {
-                // Store the current user
-                setAuthUser(user);
-                setIsLoggedIn(true);
-            }
-            else {
-                setIsLoggedIn(false);
-            }
-        });
-        // Cleanup function
-        return () => unsubscribe();
-    }, [])
-    
+    const currentUserId = useSelector((state: any) => state.users.currentUser);
+    console.log(currentUserId);
     return (
         <div>
-            {isLoggedIn ? (
+            {currentUserId ? (
                 <>
                     <p>You are logged in!</p>
                     <button onClick={() => firebaseClient.logout(firebaseAuth)}>Logout</button>
@@ -46,7 +29,7 @@ function Login() {
                             setPassword(e.target.value);
                         }}/>
                     <button onClick={() => firebaseClient.signIn(email, password)}>Login</button>
-                    <button onClick={() => firebaseClient.signUp(email, password, authUser)}>Create Account</button>
+                    <button onClick={() => firebaseClient.signUp(email, password)}>Create Account</button>
                 </>
             )}
         </div>
