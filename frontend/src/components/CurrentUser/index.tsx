07ee5ac130b1cf8bc88c776b0c236234df0b9a26
coreyan1234 <coreyan1234@gmail.com>
2024-04-17 02:03:15 -0400

    Implemented Signin component

--- frontend/src/components/CurrentUser/index.tsx
+++ frontend/src/components/CurrentUser/index.tsx
@@ -6,25 +6,37 @@ import { firebaseAuth } from "../../firebase";
 import axios from "axios";
 
 export default function CurrentUser({ children }: { children: any }) {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        // onAuthStateChanged is always listening for changes in authentication state
-        // SO, authUser will always be up to date and you can just access information
-        // about the current user via authUser
-        const unsubscribe = onAuthStateChanged(firebaseAuth, async user => {
-            if (user) {
-                // Set the current user
-                const response = await axios.get(`http://localhost:4000/api/users/6619fc391bdc52665322ec9c`);
-                console.log(`RESPONSE: ${JSON.stringify(response.data)}`);
-                dispatch(setCurrentUser(response.data));
-            }
-            else {
-                // Set current user to null
-                dispatch(setCurrentUser(null));
-            }
-        });
-        // Cleanup function
-        return () => unsubscribe();
-    }, [])
+    // const dispatch = useDispatch();
+    // useEffect(() => {
+    //     // onAuthStateChanged is always listening for changes in authentication state
+    //     // SO, authUser will always be up to date and you can just access information
+    //     // about the current user via authUser
+    //     const unsubscribe = onAuthStateChanged(firebaseAuth, async user => {
+    //         if (user) {
+    //             // Set the current user
+    //             const response = await axios.get(`http://localhost:4000/api/users/6619fc391bdc52665322ec9c`);
+    //             console.log(`RESPONSE: ${JSON.stringify(response.data)}`);
+    //             dispatch(setCurrentUser(response.data));
+    //         }
+    //         else {
+    //             // Set current user to null
+    //             dispatch(setCurrentUser(null));
+    //         }
+    //     });
+    //     // Cleanup function
+    //     return () => unsubscribe();
+    // }, [])
+
+    // const dispatch = useDispatch();
+    // const USERS_API = "http://localhost:4000/api/users";
+    // const fetchCurrentUser = async () => {
+    //     try {
+    //         const response = await axios.get(`${USERS_API}/${}`);
+    //     }
+    //     catch (error) {
+            
+    //     }
+    // }
+
     return children;
 }
