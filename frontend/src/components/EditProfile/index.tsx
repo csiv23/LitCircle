1b3014213ed1d48f993f0106af069805744c07d6
coreyan1234 <coreyan1234@gmail.com>
2024-04-18 21:02:33 -0400

    Successfully updating mongoDB with the new username and password

--- frontend/src/components/EditProfile/index.tsx
+++ frontend/src/components/EditProfile/index.tsx
@@ -1,39 +1,3 @@
-// import { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
-// import * as client from "../../client";
-
-// export default function EditProfile() {
-//     const { userId } = useParams();
-//     const [updatedUser, setUpdatedUser] = useState(null);
-
-//     console.log("EditProfile currentUser._id: " + currentUser._id);
-
-//     // Updates the user's username and password
-//     const updateProfile = async () => {
-//         await client.updateUserProfile(currentUser._id, updatedUser.Username, updatedUser.Password);
-//         console.log("Updated profile in MongoDB!");
-//     }
-
-//     return (
-//         <div>
-//             <h1>Edit Profile</h1>
-//             {userId && (
-//                 <div>
-//                     Username:
-//                     <input value={updatedUser.Username} onChange={(e) =>
-//                         setUpdatedUser({ ...updatedUser, Username: e.target.value })} />
-//                     Password:
-//                     <input value={updatedUser.Password} onChange={(e) =>
-//                         setUpdatedUser({ ...updatedUser, Password: e.target.value })} />
-//                     <br />
-//                     <br />
-//                     <button onClick={updateProfile}> Save Changes </button>
-//                 </div>
-//             )}
-//         </div>
-//     )
-// }
-
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import * as client from "../../client";
@@ -43,45 +7,42 @@ import { setCurrentUser } from "../../reducers/usersReducer";
 
 export default function EditProfile() {
     const { userId } = useParams();
-    // const [currentUser, setCurrentUser] = useState<User | null>(null);
-    const [updatedUser, setUpdatedUser] = useState<User | null>(null);
+    const currentUser = useSelector((state: any) => state.users.currentUser);
+    const [updatedUser, setUpdatedUser] = useState(currentUser);
+    const dispatch = useDispatch();
     useEffect(() => {
         const fetchUser = async () => {
             const user = await client.profile();
-            // setCurrentUser(user);
             setUpdatedUser(user);
         }
         fetchUser();
     }, []);
 
-    // console.log("EditProfile currentUser._id: " + currentUser?.userId);
+    console.log("EditProfile currentUser: " + JSON.stringify(currentUser));
+    console.log("updatedUser" + JSON.stringify(updatedUser));
 
-    const [newUsername, setNewUsername] = useState("");
-    const [newPassword, setNewPassword] = useState("");
     const navigate = useNavigate();
     // Updates the user's username and password
     const updateProfile = async () => {
-        await client.updateUserProfile(userId, newUsername, newPassword);
+        await client.updateUserProfile(userId, updatedUser.Username, updatedUser.Password);
         console.log("Updated profile in MongoDB!");
+        // dispatch(setCurrentUser(updatedUser));
         navigate(`/myProfile/${userId}`)
     }
-    const currentUserRedux = useSelector((state: any) => state.users.currentUser);
-    const dispatch = useDispatch();
-    
-    
-    console.log("CurrentUser: " + JSON.stringify(currentUserRedux));
 
+    const currUsername = updatedUser.Username;
+    const currPassword = updatedUser.Password;
     return (
         <div>
             <h1>Edit Profile</h1>
             {userId && (
                 <div>
                     Username:
-                    <input onChange={(e) =>
-                        setNewUsername(e.target.value)} />
+                    <input value={currUsername} onChange={(e) =>
+                        setUpdatedUser({...updatedUser, Username: e.target.value})} />
                     Password:
-                    <input onChange={(e) =>
-                        setNewPassword(e.target.value)} />
+                    <input value={currPassword} onChange={(e) =>
+                        setUpdatedUser({...updatedUser, Password: e.target.value})} />
                     <br />
                     <br />
                     <button onClick={updateProfile}> Save Changes </button>
