e8728ee22ef73aa672d6a8f7942bfadab7a9f34d
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2024-04-22 20:57:02 -0400

    Added frontend display for recent users

--- backend/src/routes/homepageRoutes.js
+++ backend/src/routes/homepageRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { getRecentUsers, getNewFollowers } = require('../controllers/homepageController');
 const { getUserNextMeetings} = require('../controllers/userController');
 
-router.get('/data', getRecentUsers);
+router.get('/recent-users', getRecentUsers);
 router.get('/:userId/nextmeetings', getUserNextMeetings); 
 router.get('/:userId/newfollowers', getNewFollowers);
 
--- frontend/src/components/Home/index.tsx
+++ frontend/src/components/Home/index.tsx
@@ -3,35 +3,45 @@ import { Link } from "react-router-dom";
 import "./index.css";
 import Header from "../Header";
 import background from "../images/homepage-bg.jpeg";
-import * as client from "../../mongooseClient"; // Make sure this import is correctly pointing to your client setup
-import { User, NextMeeting } from "../types"; // Assuming you've imported the correct types
+import * as client from "../../mongooseClient";
+import { User, NextMeeting } from "../types";
 
 function Home() {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
   const [nextMeetings, setNextMeetings] = useState<NextMeeting[]>([]);
   const [newFollowers, setNewFollowers] = useState<User[]>([]);
+  const [recentUsers, setRecentUsers] = useState<User[]>([]);
 
   useEffect(() => {
     const fetchProfileAndRelatedData = async () => {
       try {
-        const userSession = await client.profile(); // Fetch current user profile
-        setCurrentUser(userSession);
+        const userSession = await client.profile();
         if (userSession && userSession._id) {
+          setCurrentUser(userSession);
           const meetings = await client.getUserNextMeetings(userSession._id);
           setNextMeetings(meetings);
           const followers = await client.getNewFollowers(userSession._id);
           setNewFollowers(followers);
-
-          // Add more data fetching here if necessary
         }
       } catch (error) {
-        console.error(
-          "Failed to fetch user profile or additional data:",
-          error
-        );
-        setCurrentUser(null);
+        if ((error as any).response && (error as any).response.status === 401) {
+          // User is not authenticated, set current user to null
+          setCurrentUser(null);
+          // Fetch recent users instead
+          const recentUsersResponse = await client.getRecentUsers();
+          console.log("Recent Users Response");
+          console.log(recentUsersResponse);
+          setRecentUsers(recentUsersResponse);
+        } else {
+          console.error(
+            "Failed to fetch user profile or additional data:",
+            error
+          );
+          setCurrentUser(null);
+        }
       }
     };
+
     fetchProfileAndRelatedData();
   }, []);
 
@@ -47,7 +57,7 @@ function Home() {
           {currentUser ? (
             <>
               <h3>Welcome Back, {currentUser.username}!</h3>
-                <h4>Start organizing your club today.</h4>
+              <h4>Start organizing your club today.</h4>
             </>
           ) : (
             <>
@@ -60,48 +70,79 @@ function Home() {
           )}
         </div>
       </div>
-      {/* Display upcoming meetings */}
-      {nextMeetings.length > 0 && (
+      {/* New section for both signed-in content and recent users */}
+      {(currentUser || recentUsers.length > 0) && (
         <div className="container mt-5">
-          <h4>Upcoming Meetings:</h4>
-          <div className="row">
-            {nextMeetings.map((meeting) => (
-              <div
-                key={meeting.NextMeetingDate.toString()}
-                className="col-md-3 mb-3"
-              >
-                <div className="card">
-                  <div className="card-body">
-                    <p className="card-text">
-                      Next meeting at {meeting.NextMeetingLocation} on{" "}
-                      {new Date(meeting.NextMeetingDate).toLocaleDateString()}
-                    </p>
+          {/* Display upcoming meetings */}
+          {nextMeetings.length > 0 && (
+            <div>
+              <h4>Upcoming Meetings:</h4>
+              <div className="row">
+                {nextMeetings.map((meeting) => (
+                  <div
+                    key={meeting.NextMeetingDate.toString()}
+                    className="col-md-3 mb-3"
+                  >
+                    <div className="card">
+                      <div className="card-body">
+                        <p className="card-text">
+                          Next meeting at {meeting.NextMeetingLocation} on{" "}
+                          {new Date(meeting.NextMeetingDate).toLocaleDateString()}
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                </div>
+                ))}
               </div>
-            ))}
-          </div>
+            </div>
+          )}
+          {/* Display new followers */}
+          {newFollowers.length > 0 && (
+            <div className="mt-5">
+              <h4>New Followers:</h4>
+              <div className="row">
+                {newFollowers.map((follower) => (
+                  <div key={follower._id} className="col-md-3 mb-3">
+                    <div className="card">
+                      <div className="card-body">
+                        <Link
+                          to={`/profile/${follower._id}`}
+                          className="card-link"
+                        >
+                          {follower.username}
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+          {/* Display recent users */}
+          {recentUsers.length > 0 && (
+            <div className="mt-5">
+              <h4>Recent Users:</h4>
+              <div className="row">
+                {recentUsers.map((user) => (
+                  <div key={user._id} className="col-md-3 mb-3">
+                    <div className="card">
+                      <div className="card-body">
+                        <Link
+                          to={`/profile/${user._id}`}
+                          className="card-link"
+                        >
+                          {user.username}
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
         </div>
       )}
-      {/* Display new followers */}
-      {newFollowers.length > 0 && (
-        <div className="container mt-5">
-          <h4>New Followers:</h4>
-          <div className="row">
-            {newFollowers.map((follower) => (
-              <div key={follower._id} className="col-md-3 mb-3">
-                <div className="card">
-                  <div className="card-body">
-                    <Link to={`/profile/${follower._id}`} className="card-link">
-                      {follower.username}
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
+      {/* Buttons section */}
       <div className="home-search mt-5">
         <Link to="/search-clubs">
           <button className="btn btn-primary mr-2">Join a Club</button>
