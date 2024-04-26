a475102b925e9805b31cb535c7b55fdf44d9ec7d
Candice Mac <65557180+cmac4396@users.noreply.github.com>
2024-04-25 20:47:18 -0400

    done

--- frontend/src/components/Books/index.tsx
+++ frontend/src/components/Books/index.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import * as client from '../SearchBooks/client';
 import './index.css';
@@ -11,6 +11,7 @@ import { useSelector } from "react-redux";
 import NoBookFound from "./NoBookFound";
 
 export default function Books() {
+    const navigate = useNavigate();
     const { bookId } = useParams<string>();
     const [book, setBook] = useState<any>({});
     const [bookInBooksRead, setBookInBooksRead] = useState(false);
@@ -37,37 +38,44 @@ export default function Books() {
     const findBook = async (id: string) => {
       const googleBookData = await client.getBookDetails(id);
       console.log(googleBookData);
-      const mongooseBookId = await mongooseClient.createBook(googleBookData);
-      console.log(mongooseBookId);
 
-      const mongooseBookData = await mongooseClient.getBookById(mongooseBookId);
-      setBook(mongooseBookData as Book);
-      console.log(mongooseBookData);
-      
-
-      if (mongooseBookData) {
-        const response = await mongooseClient.getClubsReadingPerBook(mongooseBookData._id);
-        setClubsReading(response);
-
-        const userSession = await mongooseClient.profile();
-        setCurrentUser(userSession);
-
-        if (userSession) {
-           console.log(userSession);
-          if (userSession.booksRead) {
-              setBookInBooksRead(userSession.booksRead.includes(mongooseBookData._id));
-          }
+      if (googleBookData && googleBookData._id) {
+        const mongooseBookId = await mongooseClient.createBook(googleBookData);
+        console.log(mongooseBookId);
   
-          if (userSession.wishlist) {
-              setBookInWishlist(userSession.wishlist.includes(mongooseBookData._id));
-          }
+        const mongooseBookData = await mongooseClient.getBookById(mongooseBookId);
+        setBook(mongooseBookData as Book);
+        console.log(mongooseBookData);
+        
+  
+        if (mongooseBookData) {
+          const response = await mongooseClient.getClubsReadingPerBook(mongooseBookData._id);
+          setClubsReading(response);
+  
+          const userSession = await mongooseClient.profile();
+          setCurrentUser(userSession);
+  
+          if (userSession) {
+             console.log(userSession);
+            if (userSession.booksRead) {
+                setBookInBooksRead(userSession.booksRead.includes(mongooseBookData._id));
+            }
+    
+            if (userSession.wishlist) {
+                setBookInWishlist(userSession.wishlist.includes(mongooseBookData._id));
+            }
+  
+            const clubs = await mongooseClient.getUserClubsWithoutBookRec(userSession._id, mongooseBookData._id);
+            console.log("clubs without recs");
+            console.log(clubs);
+            setUserClubsWithoutRecs(clubs);
+        }
+        }
+      }
+      else {
+        navigate("/book");
+      }
 
-          const clubs = await mongooseClient.getUserClubsWithoutBookRec(userSession._id, mongooseBookData._id);
-          console.log("clubs without recs");
-          console.log(clubs);
-          setUserClubsWithoutRecs(clubs);
-      }
-      }
     };
 
   const recommendBookToClub = async (clubId : string) => {
